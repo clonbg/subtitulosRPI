d7ee9e467afbae4fb61230fe96790db70efb08c3
clonbg <clonbg@gmail.com>
2019-07-22 20:41:16 +0200

    existe es.srt?

--- supersubtitle.sh
+++ supersubtitle.sh
@@ -4,31 +4,41 @@ IFS='
 '
 
 RUTA=/media/series
-# Descargue subtitulos de todas las series
 # Instalar subliminal
 
 #########    subliminal download -l es -f $RUTA
 
-find /media/series -name *es.srt > salida.log
-
-#Sustituir ruta del espacio para ruta absoluta
+find /media/series -name *.mp4 > videos.log
+find /media/series -name *.avi >> videos.log
+find /media/series -name *.mkv >> videos.log
 
 #Leer el archivo salida.log linea a linea
 
-while read linea ; do
+while read video ; do
    #rm "$linea"
    #echo "${linea%.*}" elimina todo desde el último punto
-   nombreSinExt="${linea%.*.*}"
-   nombreSinExt="${nombreSinExt##*/}"
-   echo $nombreSinExt
-   echo $linea
-   archivoVideo="${linea%.*.*}"
-   archivoVideo+=".sincro.srt"
-   if [ -f "$archivoVideo" ]
+   #nombreSinExt="${linea%.*.*}"
+   #nombreSinExt="${nombreSinExt##*/}"
+   #archivoVideo+=".sincro.srt"
+   sincronizado="${video%.*}"
+   sincronizado+=".sincro.srt"
+   subtitulo="${video%.*}"
+   subtitulo+=".es.srt"
+
+   if [ -f "$sincronizado" ]
    then
-      echo "Existe"
+      echo "Existe $sincronizado"
+      DIA=`date +"%d/%m/%Y"`
+      HORA=`date +"%H:%M"`
+      echo "$DIA - $HORA : Existe $sincronizado" >> salida.log
    else
-      echo "No existe"
+      echo "No existe $sincronizado"
+      if [ -f "$subtitulo" ]
+      then
+         echo "Existe $subtitulo"
+      else
+         echo "No existe $subtitulo"
+      fi
    fi
-done <<< "`cat salida.log`"
+done <<< "`cat videos.log`"
 
